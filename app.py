e7bfecb9ed1c466ef5758b8bbf26bcdf825813df
Chris <caston88@gmail.com>
2023-03-24 12:50:02 +0000

    Add Plant and Room models

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 # Imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+from models import Room
+
 
 # initializing our app
 app = Flask(__name__)
 app.debug = True
+load_dotenv()
+database_url = os.getenv('DATABASE_URL')
+
 
 # Configs
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cas@cas/dont_let_them_perish'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 
 # Modules
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Models
 
-
+    
 # Schema Objects
 # Our schema objects will go here
 
@@ -25,6 +35,10 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
+    # db.create_all()
+    # kitchen = Room(name='Kitchen', plant_count=2)
+    # db.session.add(kitchen)
+    # db.session.commit()
     return 'Welcome to Don\'t let them perish API'
 if __name__ == '__main__':
      app.run()
---
+++ models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Room(db.Model):
+    __tablename__ = 'rooms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    plant_count = db.Column(db.Integer, nullable=False)
+    plants = db.relationship('Plant', backref='location')
+
+    def __init__(self, name, plant_count):
+      self.name = name
+      self.plant_count = plant_count
+
+    def __repr__(self):
+        return '' % self.id
+    
+
+class Plant(db.Model):
+    __tablename__ = 'plants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), index=True, nullable=False)
+    date_watered = db.Column(db.Date, nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'))
+
+    def __repr__(self):
+        return '' % self.name % self.date_watered % self.room_id
+  
