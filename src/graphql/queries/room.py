357fa85c4ec35bd07c6f7820c980d262e3e1d7bb
Chris <caston88@gmail.com>
2023-03-27 12:47:21 +0100

    Add GraphQL functionality to get room by name

--- src/graphql/queries/room.py
+++ src/graphql/queries/room.py
@@ -2,9 +2,15 @@ import graphene
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 
 from src.graphql.types.room import RoomType
+from src.models.models import Room as RoomModel
 
 class RoomQuery(graphene.ObjectType):
     node = graphene.relay.Node.Field()
+    
     all_rooms = SQLAlchemyConnectionField(RoomType)
+    room_by_name = graphene.Field(RoomType, room_name=graphene.String())
 
+    def resolve_room_by_name(self, info, room_name):
+        query = RoomType.get_query(info)
+        return query.filter(RoomModel.room_name == room_name).first()
 
